49640514af1cd2916c6d26aac97fd0cd83b9d5d1
snnmst <sinanmustu.sm@gmail.com>
2020-05-30 23:30:04 +0300

    First commit

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import short_urls
+from .forms import UrlForm
+from .shortner import shortner
+from django.contrib import messages
+
+
+def Sample(request, token):
+    long_url = short_urls.objects.filter(short_url=token)[0]
+    return redirect(long_url.long_url)
+
+@login_required
+def home(request):
+    form = UrlForm(request.POST)
+    a = ""
+    if request.method == 'POST':
+        if form.is_valid():
+            NewUrl = form.save(commit = False)
+            a = shortner().issue_token()
+            NewUrl.short_url = a
+            NewUrl.save()
+        else:
+            form = UrlForm()
+            messages.warning(request, f'This URL already exists or Invalid URL!')
+    return render(request, 'blog/home.html', {'form': form, 'a': a})
+
+
+# Create your views here.
